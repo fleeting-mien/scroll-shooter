1585ff9213e4e39bd94506eb1bcf5d9a72101040
arturfedoruk <mr.gebrem003@mail.ru>
2022-11-25 12:12:18 +0300

    main
    то что я успел сделать в мэине

--- main.py
+++ main.py
@@ -4,18 +4,28 @@ from config import *
 
 pygame.init()
 screen = pygame.display.set_mode((MAX_X, MAX_Y))
+game_state = "game"
+# пока что будут два состояния: "game" когда играем, "gameover" когда мы проиграли.
+# потом сделаем "menu", вместо "game" сделаем уровни и т.д. наверное
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
+enemies = []
+bullets = []
+player = Ship.Ally()
+Ship.Enemy()
+Ship.Enemy()
+
 
 while not finished:
     clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-        elif event.type == pygame.KEYDOWN:
-            pass
+    if game_state == "game": # блок действий, когда идет игра
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+            elif event.type == pygame.KEYDOWN:
+                pass
 
 
     pygame.display.update()
