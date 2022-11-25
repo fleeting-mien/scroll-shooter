cb03df3d87582a0e3349d7a3778aeff75e102446
fleeting-mien <fleeting_mien@proton.me>
2022-11-25 10:29:14 +0300

    созданы файлы, объявлены классы

---
+++ classes.py
@@ -0,0 +1,22 @@
+class Bullet:
+    pass
+
+
+class Ship:
+    pass
+
+
+class Ally(Ship):
+    pass
+
+
+class Enemy(Ship):
+    pass
+
+
+class Scene:  # ?
+    pass
+
+
+class Game_over_screen:  # ?
+    pass
