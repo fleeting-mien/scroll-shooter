276212a56220efcdfaa7e67b2a0d44ebbd1437c7
Kosiposha2007 <aldushonkov@gmail.com>
2022-11-25 22:19:53 +0300

    test

--- classes.py
+++ classes.py
@@ -1,79 +1,85 @@
 import pygame
 import sys
+from random import randint
 
 enemy_bullet_group = pygame.sprite.Group()
 
-class Bullet(pygame.sprite.Sprite):
+
+class EnemyBullet(pygame.sprite.Sprite):
     def __init__(self, picture_path):
         super().__init__()
         self.image = pygame.image.load(picture_path)
         self.rect = self.image.get_rect()
-
-
-class Enemy(Bullet):
-    def __init__(self):
-        super().__init__(self)
-        self.rect.center = enemy.rect.center
+        # self.rect.center = enemy.rect.center
         self.vy = 20    # Можно добавить ползунок сложности и менять значение скорости вражеских пуль
 
     def create(self):
-        new_enemy_bullet = Enemy("pixil-frame-0 (1).png")
+        new_enemy_bullet = EnemyBullet("pixil-frame-0 (1).png")
         enemy_bullet_group.add(new_enemy_bullet)
 
     def update(self):
         self.rect.top -= 20
 
-    def hittest(self):
-        pass
 
 
-class Ship(pygame.sprite.Sprite):
+class EnemyShip(pygame.sprite.Sprite):
     def __init__(self, picture_path):
-        super().__init__()
+        super().__init__(picture_path)
         self.image = pygame.image.load(picture_path)
         self.rect = self.image.get_rect()
         self.x = self.rect.left
         self.y = self.rect.top
 
 
-
-class Ally(Ship):
-
-    def shoot(self):
-        global ally_bullets
-        new_bullet = Bullet("pixil-frame-0 (1).png")
-
+class AllyShip(pygame.sprite.Sprite):
+    def __init__(self, picture_path):
+        super().__init__()
+        self.image = pygame.image.load(picture_path)
+        self.rect = self.image.get_rect()
+        self.x = self.rect.left
+        self.y = self.rect.top
+        self.lives = 3
 
     def update(self):
         self.rect.center = pygame.mouse.get_pos()
 
+    def hit(self):
+            for i in pygame.sprite.spritecollide(ally_ship, enemy_bullet_group, True):
+                self.lives -= 1
 
 pygame.init()
 clock = pygame.time.Clock()
-print(enemy_bullet_group)
+print(type(enemy_bullet_group), enemy_bullet_group)
 
-
+enemy = EnemyBullet("pixil-frame-0 (1).png")
+enemy.create()
 
 background = pygame.image.load("Space (1).jpg")
 
-ally_ship = Ally("pixil-frame-0 (3).png")
+ally_ship = AllyShip("pixil-frame-0 (3).png")
 ship_group = pygame.sprite.Group()
 ship_group.add(ally_ship)
-
+ship = AllyShip("pixil-frame-0 (3).png")
 screen_width = 600
 screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
-
+enemy_bullet = EnemyBullet("pixil-frame-0 (1).png")
 pygame.mouse.set_visible(False)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     screen.blit(background, (0,0))
+    ship.hit()
+    if randint(1, 10000) == 10:
+        enemy_bullet.create()
+    enemy_bullet_group.draw(screen)
     ship_group.draw(screen)
     ship_group.update()
     pygame.display.update()
+    print(ship.lives)
 
 class Scene:  # ?
     pass
