eff36d75325afcd4b82f08209802cd5011cc7d48
alexgaborak <alexgaborak@phystech.edu>
2022-12-14 23:11:35 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='scroll-shooter',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='GNU GPL',
+    author='fapg',
+    author_email='',
+    description='b02-203 1 sem game'
+)
